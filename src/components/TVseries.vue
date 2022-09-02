7040d23a989acdfc14cae6e7daff36b31440ada5
AlessioMaria5 <alessiomaria5@gmail.com>
2022-09-02 09:08:54 +0200

    refactoring star e if

--- src/components/TVseries.vue
+++ src/components/TVseries.vue
@@ -4,10 +4,9 @@
       <ul>
         <li v-for="(TVserie, index) in displayMySeries" :key="index">
         <img :src="'https://image.tmdb.org/t/p/w500/'+TVserie.backdrop_path" alt="">
-        <p>{{TVserie.original_title}}</p>
-        <p>{{TVserie.title}}</p>
+        <p>{{TVserie.original_name}}</p>
+        <p>{{TVserie.name}}</p>
         <img class="myFlag" :src="TVserie.original_language"/>
-        <p>{{TVserie.original_language==false?'displayOff':''}}</p>
         <p>{{TVserie.vote_average}}</p>
     </li>
        
